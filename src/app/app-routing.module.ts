0f958c464ef6beb74182c497465e1a4f05fb5487
zscholts <zachscholts@quickenloans.com>
2020-10-18 15:05:56 -0400

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DonutsComponent } from './donuts/donuts.component';
+import { DonutDetailComponent } from './donut-detail/donut-detail.component';
+import { FamousPeopleComponent } from './famous-people/famous-people.component';
+import { FamousPersonDetailComponent } from './famous-person-detail/famous-person-detail.component';
 
-const routes: Routes = [];
+
+const routes: Routes = 
+/*[
+  { path: 'donuts', component: DonutsComponent, 
+    children: [{ path: 'donutdetail', component: DonutDetailComponent }]},
+  { path: 'famouspeople', component: FamousPeopleComponent,
+    children: [{ path: 'famouspersondetail', component: FamousPersonDetailComponent }]},
+];*/
+
+[
+  //{ path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'donuts', component: DonutsComponent },
+  { path: 'donutdetail/:id', component: DonutDetailComponent },
+  { path: 'famouspeople', component: FamousPeopleComponent },
+  { path: 'famouspersondetail', component: FamousPersonDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DonutsComponent } from './donuts/donuts.component';
+import { DonutDetailComponent } from './donut-detail/donut-detail.component';
+import { FamousPeopleComponent } from './famous-people/famous-people.component';
+import { FamousPersonDetailComponent } from './famous-person-detail/famous-person-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DonutsComponent,
+    DonutDetailComponent,
+    FamousPeopleComponent,
+    FamousPersonDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
